2d541986ad661f13ce3a4f35aaec83c7d78ee358
Brendan <brendan@fetchrev.com>
2018-06-11 22:48:53 -0700

    added reducers

--- src/actions/app/index.js
+++ src/actions/app/index.js
@@ -2,17 +2,17 @@
  * action types
  */
 
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const GET_PRODUCT = 'GET_PRODUCT';
+export const GET_ALL_PRODUCTS          = 'GET_ALL_PRODUCTS';
+export const GET_PRODUCT               = 'GET_PRODUCT';
 
-export const GET_ALL_COLLECTIONS = 'GET_ALL_COLLECTIONS';
-export const GET_COLLECTION = 'GET_COLLECTION';
+export const GET_ALL_COLLECTIONS       = 'GET_ALL_COLLECTIONS';
+export const GET_COLLECTION            = 'GET_COLLECTION';
 
-export const CREATE_CHECKOUT = 'CREATE_CHECKOUT';
-export const GET_CHECKOUT = 'GET_CHECKOUT';
-export const GET_CHECKOUT_ID = 'GET_CHECKOUT_ID';
-export const ADD_ITEM_TO_CHECKOUT = 'ADD_ITEM_TO_CHECKOUT';
-export const UPDATE_ITEM_IN_CHECKOUT = 'UPDATE_ITEM_IN_CHECKOUT';
+export const CREATE_CHECKOUT           = 'CREATE_CHECKOUT';
+export const GET_CHECKOUT              = 'GET_CHECKOUT';
+export const GET_CHECKOUT_ID           = 'GET_CHECKOUT_ID';
+export const ADD_ITEM_TO_CHECKOUT      = 'ADD_ITEM_TO_CHECKOUT';
+export const UPDATE_ITEM_IN_CHECKOUT   = 'UPDATE_ITEM_IN_CHECKOUT';
 export const REMOVE_ITEM_FROM_CHECKOUT = 'REMOVE_ITEM_FROM_CHECKOUT';
 
 /*
